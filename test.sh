ac81451d8a11a79893202526f6aa7e731b527c7f
SamoKopecky <skopecky@redhat.com>
2023-01-19 12:20:29 +0100

    fix
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- test.sh
+++ test.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-FIRST="ghcr.io;GHCR_NAME;GITHUB_TOKEN"
+FIRST="ghcr.io;GHCR_NAME;GITHUB_TOKEN,quay.io;QUAY_NAME;QUAY_PASSWORD"
 SEC="linux/amd64,linux/arm64"
 
 JSON="{\"platform\":"
 
 JSON="$JSON$(echo -n "$SEC" | jq -csR '. | split(",")'),"
 
+JSON="$JSON\"registry\":["
+IFS=',' read -ra SET <<< "$FIRST"
+
+for i in "${SET[@]}"; do
+	IFS=';' read -ra LINE <<< "$i"
+	JSON_LINE="\"${LINE[0]}\","
+	JSON="$JSON$JSON_LINE"
+done
+
+if [[ $JSON == *, ]]; then
+	JSON="${JSON%?}"
+fi
+JSON="$JSON],"
+
+
 JSON="$JSON\"include\":["
 IFS=',' read -ra SET <<< "$FIRST"
 
-# CSV_SPLIT="$(echo -n "$1" | jq -csR '. | split(",")')"
-#
 for i in "${SET[@]}"; do
 	IFS=';' read -ra LINE <<< "$i"
 	JSON_LINE="{\"registry\": \"${LINE[0]}\", \"username\": \"${LINE[1]}\", \"password\": \"${LINE[2]}\"},"	

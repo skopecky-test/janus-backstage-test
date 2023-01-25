d1e3e356895c9a83de68ad5846fb9673f54646ad
SamoKopecky <skopecky@redhat.com>
2023-01-25 16:08:39 +0100

    fix
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- test.sh
+++ test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-TEST=$(echo linux/amd64,linux/arm64 | sed  -e 's/\//-/g')
-TEST2=$(echo -n linux/amd64,linux/arm64 | jq -csR '. | split(",")')
-TEST3=$(echo linux/amd64 | sed  -e 's/\//-/g')
-echo $TEST
-echo $TEST2
-echo $TEST3
+for TEST in latest latest; do
+	echo $TEST
+done
